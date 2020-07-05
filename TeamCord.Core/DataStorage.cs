a6714f6244d44c06ef6d7441f3499fbbfe26ce12
Kleinrotti <kleinrotti@gmail.com>
2020-07-05 20:28:26 +0200

    updated config path, added formatting to json

--- TeamCord.Core/DataStorage.cs
+++ TeamCord.Core/DataStorage.cs
@@ -6,12 +6,12 @@ namespace TeamCord.Core
 {
     public class DataStorage
     {
-        private static string _configPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Teamcord\config";
+        private static string _configPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\TS3Client\plugins\TeamCord\";
 
         public DataStorage()
         {
             Directory.CreateDirectory(_configPath);
-            if (!File.Exists(_configPath + @"\config.json"))
+            if (!File.Exists(_configPath + @"config.json"))
             {
                 Logging.Log("Config file does not exists, creating one...");
                 StoreSettings(new PluginSettings());
@@ -21,14 +21,14 @@ namespace TeamCord.Core
         public void StoreSettings(PluginSettings settings)
         {
             Logging.Log("Storing plugin settings...", LogLevel.LogLevel_INFO);
-            var json = JsonConvert.SerializeObject(settings);
-            File.WriteAllText(_configPath + @"\config.json", json);
+            var json = JsonConvert.SerializeObject(settings, Formatting.Indented);
+            File.WriteAllText(_configPath + @"config.json", json);
         }
 
         public PluginSettings GetSettings()
         {
             Logging.Log("Reading plugin settings...", LogLevel.LogLevel_INFO);
-            var jsonString = File.ReadAllText(_configPath + @"\config.json");
+            var jsonString = File.ReadAllText(_configPath + @"config.json");
             return JsonConvert.DeserializeObject<PluginSettings>(jsonString);
         }
     }
