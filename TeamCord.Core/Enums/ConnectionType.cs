8678b73d37dc8b4deed8261f7247cc67d205ef28
Kleinrotti <kleinrotti@gmail.com>
2020-08-01 14:26:04 +0200

    small refactorings

---
+++ TeamCord.Core/Enums/ConnectionType.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamCord.Core
+{
+    /// <summary>
+    /// Indicates what type of connection is established/destablished
+    /// </summary>
+    public enum ConnectionType
+    {
+        Voice,
+        Text,
+        Login,
+        Logout
+    }
+}
