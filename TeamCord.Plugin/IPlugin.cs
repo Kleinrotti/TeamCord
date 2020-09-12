fa7fc16d98da35b810429fc42d2dc47ad709cedf
Kleinrotti <kleinrotti@gmail.com>
2020-09-12 23:39:25 +0200

    added plugin interface

---
+++ TeamCord.Plugin/IPlugin.cs
@@ -0,0 +1,53 @@
+﻿using TeamCord.Core;
+
+namespace TeamCord.Plugin
+{
+    internal interface IPlugin
+    {
+        /// <summary>
+        /// Settings of the plugin
+        /// </summary>
+        PluginSettings Settings { get; }
+
+        /// <summary>
+        /// Plugin name
+        /// </summary>
+        string PluginName { get; }
+
+        /// <summary>
+        /// Current plugin version
+        /// </summary>
+        string PluginVersion { get; }
+
+        /// <summary>
+        /// Teamspeak API Version of the plugin
+        /// </summary>
+        int ApiVersion { get; }
+
+        /// <summary>
+        /// Plugin author
+        /// </summary>
+        string Author { get; }
+
+        /// <summary>
+        /// Plugin description
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// Unique plugin ID
+        /// </summary>
+        string PluginID { get; set; }
+
+        /// <summary>
+        /// Initialization function
+        /// </summary>
+        /// <returns> 0 for success, 1 for failure</returns>
+        int Init();
+
+        /// <summary>
+        /// Shutdown function
+        /// </summary>
+        void Shutdown();
+    }
+}
